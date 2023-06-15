a1fabd73aca14f773527cf40dc527d6046d38418
Sarguru02 <psgnhealth@gmail.com>
2023-06-15 16:13:39 +0530

    fixed style

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-let ejs = require('ejs');
-const port = 3000
-const path = require('path')
+let ejs = require("ejs");
+const port = 3000;
+const path = require("path");
 
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get('/', (req, res)=> {
-  res.render('pages/index');
+app.get("/", (req, res) => {
+  res.render("pages/index");
 });
 
 app.listen(3000);
-console.log('Server is listening on port 3000\nhttp://localhost:3000/ ');
+console.log("Server is listening on port 3000\nhttp://localhost:3000/ ");
