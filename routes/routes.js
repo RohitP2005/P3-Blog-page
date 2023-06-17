804e2ab259a54459bdb36b5b38393212c5ea99a0
RohitP2005 <rohitthevar@gmail.com>
2023-06-17 17:55:00 +0530

    17/06/2023

--- routes/routes.js
+++ routes/routes.js
@@ -16,7 +16,21 @@ router.get("/about", (req, res) => {
 router.get("/blogs", (req, res) => {
     res.render("pages/blogs",blogs);
 });
-
+router.get("/add", (req, res) => {
+    res.render("pages/add");
+});
+router.post("/add", (req, res) => {
+    var ids = blogs.length
+    const post = ({
+      title: req.body.title,
+      author: req.body.author,
+      content: req.body.content,
+      id:ids+1
+    });
+    blogs.push(post)
+    res.redirect("/");
+  });
+  
 
 module.exports = router
 
