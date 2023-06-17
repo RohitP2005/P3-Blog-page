69801b058d41daf425909298c982a745fa414d9b
RohitP2005 <rohitthevar@gmail.com>
2023-06-18 03:17:33 +0530

    2nd commit 18/06/2023

--- routes/routes.js
+++ routes/routes.js
@@ -14,34 +14,42 @@ router.get("/about", (req, res) => {
     res.render("pages/about");
 });
 router.get("/blogs", (req, res) => {
-    res.render("pages/blogs",blogs);
+    res.render("pages/blogs", blogs);
+});
+router.get("/del", (req, res) => {
+    res.redirect("/blogs");
 });
 router.get("/add", (req, res) => {
     res.render("pages/add");
 });
+router.get("/blogs/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = blogs.findIndex(obj => obj.id === id);
+    res.render("pages/pages", { blog: blogs[index] });
+});
 router.post("/add", (req, res) => {
     var ids = blogs.length
     const post = ({
-      title: req.body.title,
-      author: req.body.author,
-      content: req.body.content,
-      id:ids+1
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content,
+        id: ids + 1
     });
     blogs.push(post)
-    res.redirect("/blog");
+    res.redirect("/blogS");
 });
 router.post('/delete/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const index = blogs.findIndex(obj => obj.id === id);
     if (index !== -1) {
         blogs.splice(index, 1);
-          console.log("Object deleted successfully!");
-        } else {
-            console.log("Object not found.");
-        }
-        res.redirect("/blogs");
-  });
-  
+        console.log("Object deleted successfully!");
+    } else {
+        console.log("Object not found.");
+    }
+    res.redirect("/blogs");
+});
+
 
 module.exports = router
 
